088ef72a4512ba78770945686cf8804befe6c23e
naveen-duggal <84651021+naveen-duggal@users.noreply.github.com>
2022-10-26 22:06:42 +0530

    first commit

---
+++ Program2.java
@@ -0,0 +1,17 @@
+public class Program2
+{
+    public static void main(String[] args)
+    {
+        int i,j;
+        int n=4;
+        for(i=1;i<=4;i++)
+        {
+            for(j=1;j<=4;j++)
+            {
+                System.out.print(" "+i);
+            }
+            System.out.println();
+        }
+    }
+    
+}
